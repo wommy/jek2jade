66200413bf624bb0a022e216d00ddec499c99a7e
tommy williams <tom@technomad.media>
2015-10-05 02:11:31 -0400

    0.5.0
    added sass
    
    adding data next

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var jade = require('gulp-jade');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-// var sass = require('gulp-sass');
+var sass = require('gulp-sass');
 
 // rune this task by typing in  gulp jade  in CLI
 gulp.task('jade', function() {
@@ -16,18 +16,16 @@ gulp.task('jade', function() {
 // create a task that ensures the `js` task is complete before reloading browsers
 gulp.task('jade-watch', ['jade'], reload);
 
-gulp.task('serve', ['jade'], function() {
-  browserSync({server: './out'});
-
-  gulp.watch("in/current/**/*.jade", ['jade-watch']);
+gulp.task('sass', function(){
+  return gulp.src('./in/current/assets/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('out/assets/css')) // tells gulp our output folder
+    .pipe(reload({stream: true}));
 });
 
-// rune this task by typing in  gulp sass  in CLI
-// gulp.task('sass', function() {
-//   return gulp.src('in/current/assets/style/sass/*.scss')
-//     .pipe(sass({
-//       errLogToConsole: true,
-//       outputStyle: 'expanded'
-//     })) // pipe to jade plugin
-//     .pipe(gulp.dest('out/assets/css')); // tells gulp our output folder
-// });
+gulp.task('serve', ['jade', 'sass'], function() {
+  browserSync({server: './out'});
+
+  gulp.watch('./in/current/assets/scss/*.scss', ['sass']);
+  gulp.watch("./in/current/**/*.jade", ['jade-watch']);
+});
